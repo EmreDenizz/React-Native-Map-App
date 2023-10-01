25af5a120eb61bea30725a26609b83f099a25259
Emre Deniz <emre.deniz@outlook.com>
2023-10-01 18:54:12 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react"
+import { Dimensions, StyleSheet, View } from "react-native"
+import MapView, { } from "react-native-maps"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={{ marginTop: 50, flex: 1 }}>
+			<MapView
+				style={styles.map}
+                minZoomLevel={7}
+				initialRegion={{
+					latitude: 43.6510,
+		            longitude: -79.347,
+					latitudeDelta: 0.8,
+					longitudeDelta: 0.8
+				}}
+				provider="google"
+			/>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+	map: {
+		width: Dimensions.get("window").width,
+		height: Dimensions.get("window").height
+	}
+})
